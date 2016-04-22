a71c258ed6e0261e825a2740dddb90ab637a813c
weslord <weslord@gmail.com>
2016-04-22 06:14:28 -0700

    Basic loop and test vars to listing.php

--- listing.php
+++ listing.php
@@ -1,17 +1,45 @@
-<?php ?>
+<?php
+  $series = array(
+    "title" => "Confirmation Bias",
+    "description" => "Description of podcast goes here.",
+  );
+
+  $episodes = array(
+    array(
+      "number" => 2,
+      "date" => "Friday",
+      "title" => "Two's a crowd",
+      "description" => "Starting to get crowded in here",
+    ),
+    array(
+      "number" => 1,
+      "date" => "2015-12-02",
+      "title" => "Inauguration",
+      "description" => "It's the first episode",
+    ),
+  );
+?>
 <div id="podpub-episode-listing">
   <div id="podpub-series-info">
-    <h1>Podcast Name</h1>
-    <p>Podcast long description</p>
+    <h1><?= $series['title']; ?></h1>
+    <p><?= $series['description']; ?></p>
+    <a href="feed.rss">RSS feed</a>
   </div>
+
   <div id="podpub-episodes">
-    <div id="podpub-ep-##" class="podpub-episode">
-      <h2>Episode Title</h2>
-      <p>Episode description</p>
+
+    <?php
+      // need to sort db query as descending by date (or by episode number?)
+      foreach ($episodes as $episode) {
+    ?>
+
+    <div id="podpub-ep-<?= $episode['number'] ?>" class="podpub-episode">
+      <h2><?= $episode['number'] ?> - <?= $episode['title'] ?></h2>
+      <p>Released <?= $episode['date'] ?></p>
+      <p><?= $episode['description'] ?></p>
     </div>
-    <div id="podpub-ep-##" class="podpub-episode">
-      <h2>Episode Title</h2>
-      <p>Episode description</p>
-    </div>
+
+    <?php } ?>
+
   </div>
 </div>
